6d7966669bdc96a7fe108e46d2f7c49f7da86677
Sammra Batool <Sammrabatool.786@gmail.com>
2019-01-24 16:25:15 +0500

    reminder complete

--- app/src/main/java/com/example/sammrabatool/solutions5d/dashboard/DashboardGridFab.java
+++ app/src/main/java/com/example/sammrabatool/solutions5d/dashboard/DashboardGridFab.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -85,6 +86,16 @@ public class DashboardGridFab extends AppCompatActivity {
         customer=(TextView)findViewById(R.id.customer);
         requisition=(TextView)findViewById(R.id.requisition);
         btnbell=(GifImageButton)findViewById(R.id.APPLY);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
 
         SharedPreferences sharedprefSignup = getSharedPreferences("SignupPref", Context.MODE_PRIVATE);
 
